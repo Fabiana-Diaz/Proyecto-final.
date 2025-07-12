0fa1dcbdb6bc0078982bdf95a1f4871cf6e57b17
DANIELA CARVAJALINO <dhayhib.carvajalino@udea.edu.co>
2025-07-12 11:00:21 -0500

    Update jugador.h

--- jugador.h
+++ jugador.h
@@ -1,26 +1,43 @@
 #ifndef JUGADOR_H
 #define JUGADOR_H
 
-#include "personaje.h"
 #include <QObject>
 #include <QGraphicsPixmapItem>
 #include <QKeyEvent>
+#include <QTimer>
+#include <QList>
+#include "personaje.h"
 
-class jugador : public QObject, public personaje {
+class Plataforma;  // Declaración adelantada
+
+class jugador : public QObject, public QGraphicsPixmapItem, public personaje {
+
     Q_OBJECT
+    friend class GameScene;  // Permite acceso a enPlataforma
 
 public:
     jugador(float x, float y);
-
-    QGraphicsPixmapItem* getSprite();
-    void keyPressEvent(QKeyEvent* event);
+    void keyPressEvent(QKeyEvent* event) override;
+    void keyReleaseEvent(QKeyEvent* event) override;
     void aplicarFisica();
+    void resolverColision(QGraphicsItem* plataforma);
+    void activarCaidaLibre();
+    void verificarColisionInicial(const QList<Plataforma*>& plataformas);
+    void saltar();
+    void moverDerecha();
+    void moverIzquierda();
+    void recibirDanio(int cantidad);
 
-private:
-    float velocidadY;
-    bool enElAire;
-    QGraphicsPixmapItem* sprite;
-};
+    // --- NUEVO: bloqueo de física y sprite por victoria ---
+    void setVictoria(bool valor) { victoriaMostrada = valor; }
+    bool isVictoria() const { return victoriaMostrada; }
 
-#endif // JUGADOR_H
+    QPainterPath shape() const override;
+    bool isEnElAire() const { return enElAire; }
 
+    int getVida() const { return vida; }
+    bool isMuerto() const { return estaMuerto; }
+    void setTextoVida(QGraphicsTextItem* txt) { textoVida = txt; }
+    void setBarraVida(QGraphicsRectItem* barra) { barraVida = barra; }
+
+     void mostrarVictoria();
