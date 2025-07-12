186f7b5364bd5b6c906b27491441def8c88f2308
DANIELA CARVAJALINO <dhayhib.carvajalino@udea.edu.co>
2025-07-12 11:14:48 -0500

    Add files via upload

---
+++ GameScene2.h
@@ -0,0 +1,12 @@
+#ifndef GAMESCENE2_H
+#define GAMESCENE2_H
+
+#include <QGraphicsScene>
+
+class GameScene2 : public QGraphicsScene {
+    Q_OBJECT
+public:
+    explicit GameScene2(QObject *parent = nullptr);
+};
+
+#endif // GAMESCENE2_H
