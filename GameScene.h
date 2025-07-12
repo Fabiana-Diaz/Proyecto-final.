ddb23d7a4bdba4a5fb8cbeca7b67e1d7f0e6921f
DANIELA CARVAJALINO <dhayhib.carvajalino@udea.edu.co>
2025-07-12 10:58:56 -0500

    Update GameScene.h

--- GameScene.h
+++ GameScene.h
@@ -2,17 +2,56 @@
 #define GAMESCENE_H
 
 #include <QGraphicsScene>
-#include "goku.h"  // Clase Goku personalizada
+#include <QList>
+#include <QTimer>
+#include "jugador.h"
+#include "Plataforma.h"
+
+// 👇 Declaración adelantada para evitar error de tipo
+class Banglades;
 
 class GameScene : public QGraphicsScene
 {
     Q_OBJECT
+
 public:
     explicit GameScene(QObject *parent = nullptr);
+    jugador* getJugadorGoku() const { return jugadorGoku; }
+
+
+
+
+public slots:
+    void mostrarVictoria();
+    void focusGoku();
+
 
 private:
     jugador* jugadorGoku;
+    QList<Plataforma*> plataformas;
+    QTimer* timerVendas;
+    Banglades* enemigoBanglades;
+
+    void lanzarVendaDesdeBanglades();  // función para lanzar vendas desde el enemigo
+    QGraphicsTextItem* textoVidaGoku;
+    QGraphicsTextItem* textoVidaBanglades;
+
+    QGraphicsRectItem* barraVidaGoku;
+    QGraphicsRectItem* barraVidaBanglades;
+
+
+private slots:
+    void detenerEnemigosYVendas();
+
+
+    // Al final de tu clase GameScene
+protected:
+    void keyPressEvent(QKeyEvent* event) override;
+    void keyReleaseEvent(QKeyEvent* event) override;
+
+
 
 };
 
 #endif // GAMESCENE_H
+
