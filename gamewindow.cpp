006ed0cfe8a9b58eb9f8fb2c632e73ba31d3b0d0
DANIELA CARVAJALINO <dhayhib.carvajalino@udea.edu.co>
2025-07-13 19:41:48 -0500

    Delete gamewindow.cpp

--- gamewindow.cpp
+++
@@ -1,8 +0,0 @@
-#include "GameWindow.h"
-
-GameWindow::GameWindow(QWidget *parent)
-    : QGraphicsView(parent) {
-    setFixedSize(800, 600);
-    GameScene *scene = new GameScene();
-    setScene(scene);
-}
